73b7bb5499881dc5b01d2041034366040dd19f04
Ismael Ordoñez <orismael31@gmail.com>
2023-10-02 20:30:26 -0500

    Terminar las operaciones CRUD y layout de la pagina

--- router.js
+++ router.js
@@ -15,12 +15,38 @@ router.get('/',(req,res)=>{
     })
 })
 
-//Ruta registros    
+//Ruta crear registros    
 router.get('/create',(req, res) =>{
     res.render('create');
 })
 
+//Ruta editar registros
+router.get('/edit/:id',(req, res)=>{
+    const id = req.params.id;
+    conexion.query('SELECT * FROM users WHERE id=?',[id],(error,results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('edit', {user:results[0]});
+        }
+    })
+})
+
+//Ruta para eliminar
+router.get('/delete/:id',(req,res)=>{
+    const id = req.params.id;
+    conexion.query('DELETE FROM users WHERE id = ?',[id],(error,results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/');
+        }
+    })
+    
+})
+
 const crud = require('./controllers/crud');
-router.post('/save',crud.save)
+router.post('/save',crud.save);
+router.post('/update',crud.update);
 
 module.exports = router;
